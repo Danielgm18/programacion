08f058a375aec1619445fe1ce75d20747b61b79a
Danielgm18 <yhgomezm@unal.edu.co>
2025-09-14 22:43:23 -0500

    solucion

--- Puntos_Pag_40.py
+++ Puntos_Pag_40.py
@@ -1,1 +1,18 @@
 #Autor: Daniel Gómez Micán
+#1.Elabore un programa que dada una letra cuente cuantas ocurrencias de esta letra hay.
+
+text=input("Ingrese un texto:")
+Text1=input("Ingresa la letra a buscar:")
+A=text.count(Text1)
+print(f"La letra {Text1} aparece {A} en el texto" )
+
+#2.Elabore un programa que dada una cadena diga si todos los sımbolos de la cadena son letras.
+Text= input("Ingrese una cadena:")
+if  Text.isalpha():
+    print("Todo la cadena son letras")
+else:
+    print("No toda la cadena son letras")
+
+#3.Elabore un programa que dada una cadena cuente las consonantes en dicha cadena.
+TEXT=("Ingrese un texto")
+
