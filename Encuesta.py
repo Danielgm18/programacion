73d66839b39076fd93c1df30c4a605efe9421f10
Danielgm18 <yhgomezm@unal.edu.co>
2025-09-17 00:35:34 -0500

    Encuesta

---
+++ Encuesta.py
@@ -0,0 +1,7 @@
+#Autor: Daniel Gomez Mican
+for i in range(10):
+    N=input("Nombre:")
+    E=input("Edad:")
+    print("Hola",  N)
+    P=input("Cuentame que proyecto tienes pensado para programación:")
+    print("Que interesante, gracias por compartir tu idea")
